6e5ce6c25e234940c091d409039c18e33099920b
Stiven988 <taki00miyamizu@gmail.com>
2023-05-01 18:29:58 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,30 @@
+function Copiar1() {
+    let texto1 = document.getElementById("textp");
+    texto1.select();
+    texto1.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+}
+function Copiar2() {
+    let texto2 = document.getElementById("textp");
+    texto2.select();
+    texto2.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+}
+function Copiar3() {
+    let texto3 = document.getElementById("textp");
+    texto3.select();
+    texto3.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+}
+function Copiar4() {
+    let texto4 = document.getElementById("textp");
+    texto4.select();
+    texto4.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+}
+function Copiar5() {
+    let texto5 = document.getElementById("textp");
+    texto5.select();
+    texto5.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+}
